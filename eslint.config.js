b7e964169b888f6af1d05e15079bbc622f17504a
Aron Visky <aronvisky@gmail.com>
2026-04-28 12:40:29 +0300

    linting added

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    ignores: ['node_modules/**'],
+  },
+];
